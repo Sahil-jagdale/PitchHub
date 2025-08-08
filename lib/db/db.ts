c3f949ac1295920f24d71e2d46af031448946406
Sahil Jagdale <sahiljagdale1073@gmail.com>
2025-08-09 01:54:15 +0530

    DB Connected

--- lib/db/db.ts
+++ lib/db/db.ts
@@ -1,27 +1,32 @@
-import mongoose from 'mongoose'
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { DB_NAME } from "@/utils/constants";
+dotenv.config();
 
 type ConnectionObject = {
-    isConnected?: number
-}
+  isConnected?: number;
+};
 
-const connection: ConnectionObject = {}
+const connection: ConnectionObject = {};
 
+async function connectDB(): Promise<void> {
+  const MONGODB_URI = `${process.env.MONGODB_URI}/${DB_NAME}`;
 
-async function connectDB(): Promise<void> { 
-    if(connection.isConnected){
-        console.log("Already connected to Database.");
-        return;
-    }
-    try {
-        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI ||  '', {});
-        connection.isConnected = connectionInstance.connections[0].readyState
-        console.log(`DB connection successfully`);
-    } catch (error) {
-        console.log('Database  connection failed', error);
-        process.exit(1);
-    }
+  if (!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable");
+  }
+  if (connection.isConnected) {
+    console.log("Already connected to Database.");
+    return;
+  }
+  try {
+    const connectionInstance = await mongoose.connect(MONGODB_URI || "", {});
+    connection.isConnected = connectionInstance.connections[0].readyState;
+    console.log(`DB connection successfully`);
+  } catch (error) {
+    console.log("Database  connection failed", error);
+    process.exit(1);
+  }
 }
 
 export default connectDB;
-
